a7e34832840c9e922d37d38828f329dfd1161adc
Marcel Nogueira and Zach Bastian <zach@>
2017-03-21 14:20:48 -0700

    it will add panels, scripts, and styles.

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  // $(".script1").click(function() {
+  //     $(".answer1").toggle();
+  // });
+  // $(".answer1").click(function() {
+  //     $(".script1").toggle();
+  //   });
+
+  $(".script1").click(function() {
+    $(".answer1").show();
+    $(".script1").hide();
+  });
+
+  $(".answer1").click(function() {
+    $(".script1").show();
+    $(".answer1").hide();
+  });
+
+
+
+
+
+
+
+
+
+
+});
